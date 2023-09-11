d3128f1613d3f9a3a9683b9965a456dd1a5b0a5b
Hallison Brancalhão <hallison.o.b@gmail.com>
2023-09-11 17:04:26 -0300

    feat: 🎸 implementando método de autenticação

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(email: string, senha: string): Promise<any> {
+    const user = await this.userService.findUser(email);
+    const validPassword: boolean = await bcrypt.compare(senha, user.password);
+
+    if (!validPassword) throw new UnauthorizedException();
+
+    const payload = {
+      cpf: user.cpf,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user: user,
+    };
+  }
+}
