41247d68337cb29bca7d3796c42febc2f15e9365
Hallison Brancalhão <hallison.o.b@gmail.com>
2023-09-11 17:06:56 -0300

    feat: 🎸 configurando módulos integrados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { usersProviders } from './user.providers';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
